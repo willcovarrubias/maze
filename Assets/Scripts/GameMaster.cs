cd7cbf97c6aebb0f84d81d61830da6f0db18db20
willcovarrubias <willcovarrubias83@gmail.com>
2018-07-18 15:38:56 -0700

    Ignores

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gameMaster;
+
+    public void Awake()
+    {
+        if (gameMaster == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameMaster = this;
+        }
+        else if (gameMaster != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
