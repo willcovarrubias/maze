5a8f534f0c9f30a53abd1086ad6a9300acf4bf56
willcovarrubias <willcovarrubias83@gmail.com>
2018-07-25 08:53:30 -0700

    ItemSlots script small update

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour { //, IDropHandler {
+public class ItemSlot : MonoBehaviour, IDropHandler {
 
     public int id;
     GameObject gameMaster;
