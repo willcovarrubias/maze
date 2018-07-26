65f77f4121a4b1e96e84114372775cda361c17cb
Brandon Gomez <brlgomez@ucsc.edu>
2018-07-25 21:01:28 -0700

    Small additions to dynamic chests

--- Assets/Scripts/LootGenerator.cs
+++ Assets/Scripts/LootGenerator.cs
@@ -12,22 +12,27 @@ public class LootGenerator : MonoBehaviour
 
     void Start()
     {
-        int randomAmount = Random.Range(1, 4);
+        int randomAmount = Random.Range(1, 5);
         chests = new List<GameObject>();
+        int offset = 0;
+        if (randomAmount > 1)
+        {
+            offset = randomAmount * 50;
+        }
         for (int i = 0; i < randomAmount; i++)
         {
             GameObject newChest = Instantiate(chest, canvas.transform, true);
             newChest.AddComponent<Loot>();
             newChest.GetComponent<Loot>().RunRandom(slot, itemPrefab);
-            newChest.transform.position = new Vector3(
-                chest.transform.position.x + (i * 100),
-                chest.transform.position.y,
-                chest.transform.position.z
+            newChest.transform.localPosition = new Vector3(
+                chest.transform.localPosition.x + (i * 100) - offset,
+                chest.transform.localPosition.y,
+                chest.transform.localPosition.z
             );
-            newChest.transform.Find("ChestPanel").transform.position = new Vector3(
-                newChest.transform.Find("ChestPanel").transform.position.x - (i * 100),
-                newChest.transform.Find("ChestPanel").transform.position.y,
-                newChest.transform.Find("ChestPanel").transform.position.z
+            newChest.transform.Find("ChestPanel").transform.localPosition = new Vector3(
+                newChest.transform.Find("ChestPanel").transform.localPosition.x - (i * 100) + offset,
+                newChest.transform.Find("ChestPanel").transform.localPosition.y,
+                newChest.transform.Find("ChestPanel").transform.localPosition.z
             );
             chests.Add(newChest);
         }
