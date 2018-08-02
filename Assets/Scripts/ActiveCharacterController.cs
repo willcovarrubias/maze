8d70f9888e5eee778b3e0afb76a0e942b5efcb57
willcovarrubias <willcovarrubias83@gmail.com>
2018-08-02 10:41:27 -0700

    ActiveCharacter EXP Testing

--- Assets/Scripts/ActiveCharacterController.cs
+++ Assets/Scripts/ActiveCharacterController.cs
@@ -22,9 +22,24 @@ public class ActiveCharacterController : MonoBehaviour {
     {
         nameTextObject.text = GameMaster.gameMaster.GetComponent<CharacterDatabase>().activeCharacter.name +
                                 "\n" + GameMaster.gameMaster.GetComponent<CharacterDatabase>().activeCharacter.job +
+                                "\nXP: " + GameMaster.gameMaster.GetComponent<CharacterDatabase>().activeCharacter.exp +
                                 "\nHP: " + GameMaster.gameMaster.GetComponent<CharacterDatabase>().activeCharacter.hp +
                                 "\nMP: " + GameMaster.gameMaster.GetComponent<CharacterDatabase>().activeCharacter.mp;
 
     }
-	
+
+    public void GiveExpForRoomClearToActiveCharacter()
+    {
+        GameMaster.gameMaster.GetComponent<CharacterDatabase>().activeCharacter.exp += 100;
+        UpdateActiveCharacterVisuals();
+        GameMaster.gameMaster.Save();
+    }
+
+    public void GiveExpForBattleToActiveCharacter()
+    {
+        GameMaster.gameMaster.GetComponent<CharacterDatabase>().activeCharacter.exp += 200;
+        UpdateActiveCharacterVisuals();
+        GameMaster.gameMaster.Save();
+    }
+
 }
