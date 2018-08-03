8fc6cb8e9af6e98517c36a8f6dbff5bfd7e4c0b6
willcovarrubias <willcovarrubias83@gmail.com>
2018-08-03 09:39:01 -0700

    Fight Scene Stuff

--- Assets/Scripts/FightSceneController.cs
+++ Assets/Scripts/FightSceneController.cs
@@ -5,11 +5,71 @@ using UnityEngine.SceneManagement;
 
 public class FightSceneController : MonoBehaviour {
 
+    Character enemy1, enemy2;
+    List<Character> listOfEnemies;
+
+
+     
+    private bool onOffense;
+    private bool isFighting;
+
     private void Start()
     {
         Debug.Log("You're in the Fight scene!");
+        listOfEnemies = GameMaster.gameMaster.GetComponent<CharacterDatabase>().GetListofEnemies();
+
     }
 
+    private void Update()
+    {
+        if (isFighting)
+        {
+            if (onOffense)
+            {
+                //Player's turn to attack with meter.
+                //Meter UI pops up. It moves up and down.
+                //Player taps to stop the meter. His damage is calculated.
+                //Enemy's vitals are updated.
+                //Message is displayed for how much damage was dealt.
+                //Check if 1 enemy is still alive, if so set onOffense to false. If not, isFighting is false;
+                //onOffense is set to false.
+            }
+            else
+            {
+                //Player's turn to defense (with stars).
+                //Calculate number of attacks.
+                //Screen dims.
+                //Begin timer.
+                //Star UI pops up depending on timer (randomization?)
+                //Player taps star(s) and their damage taken is calculated.
+                //Character's vitals are  updated.
+                //Results are displayed to the player.
+                //onOffense is set to true.
+
+            }
+        }
+        //Determine rewards, calculate EXP, etc.
+        //End fight.?
+
+        
+    }
+
+    void EnemyAttacks()
+    {
+        
+    }
+
+    void LoadEnemies()
+    {
+        //Come up with algorithm here to determine amount of enemies and which enemies.
+        enemy1 = listOfEnemies[0];
+        enemy2 = listOfEnemies[1];
+    }
+
+    void CalculateEnemyAttackRounds()
+    {
+
+    }
     public void Attack()
     {
     }
